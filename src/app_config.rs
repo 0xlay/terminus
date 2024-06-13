fa5ecf832b96b3bf572d01577a520f39b4cbe64d
0xlay <zeroxlay@protonmail.com>
2024-06-13 21:08:18 +0300

    feat: add app_config module

---
+++ src/app_config.rs
@@ -0,0 +1,10 @@
+use egui::Vec2;
+
+// TODO: add terminus.json config for some fields.
+
+pub const APP_NAME: &str = "Terminus";
+
+pub const APP_ICON: &'static [u8] = include_bytes!("../assets/icons/terminus-256.png");
+
+pub const APP_DEFAULT_SIZE: Vec2 = Vec2 { x: 800.0, y: 600.0 };
+pub const APP_DEFAULT_MIN_SIZE: Vec2 = Vec2 { x: 400.0, y: 300.0 };
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use eframe::egui;
+mod app_config;
 
-const APP_NAME: &str = "Terminus";
+use eframe::egui;
 
 #[derive(Default)]
 struct Terminus {}
@@ -22,12 +22,16 @@ impl eframe::App for Terminus {
 fn main() {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([800.0, 600.0]) // TODO: load default size from the terminus.json config.
-            .with_min_inner_size([400.0, 300.0]), // TODO: also load frome the config file
+            .with_inner_size(app_config::APP_DEFAULT_SIZE)
+            .with_min_inner_size(app_config::APP_DEFAULT_MIN_SIZE)
+            .with_icon(
+                eframe::icon_data::from_png_bytes(app_config::APP_ICON)
+                    .unwrap(),
+            ),
         ..Default::default()
     };
     let _ = eframe::run_native(
-        APP_NAME,
+        app_config::APP_NAME,
         native_options,
         Box::new(|cc| Box::new(Terminus::new(cc))),
     );
